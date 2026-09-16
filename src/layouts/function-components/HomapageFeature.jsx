6e1c21aaf81206421befb49aaeca5d26aa98bb5f
wsczqtb <43517653@qq.com>
2026-09-16 14:55:57 +0800

    Update HomapageFeature.jsx

--- src/layouts/function-components/HomapageFeature.jsx
+++ src/layouts/function-components/HomapageFeature.jsx
@@ -8,18 +8,22 @@ const HomapageFeature = ({ feature_list }) => {
         return (
           <div
             key={i}
-            className="flex flex-col justify-between rounded-lg bg-white p-5 shadow-lg"
+            className="rounded-lg bg-white p-5 shadow-lg"
           >
-            <div>
-              <h3 className="h4 text-xl lg:text-2xl">{item.title}</h3>
-              <p>{item.content}</p>
-            </div>
-            <span className="icon mt-4">
+            {/* 标题行：小图标 + 标题 同一行 */}
+            <div className="flex items-center space-x-2">
               <DynamicIcon
                 icon={item.icon}
-                className="mt-4 w-6 h-6 text-primary"
+                className="h-5 w-5 shrink-0 text-primary"
               />
-            </span>
+              <h3 className="h4 text-xl lg:text-2xl">{item.title}</h3>
+            </div>
+
+            {/* 描述行：前面加小圆点 */}
+            <p className="mt-3 flex items-start text-text">
+              <span className="mr-2 mt-2 inline-block h-1.5 w-1.5 shrink-0 rounded-full bg-primary" />
+              <span>{item.content}</span>
+            </p>
           </div>
         );
       })}
